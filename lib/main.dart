1d7313ba3779f622b91267a7f70ef35aa134bc7e
Juan Luis Casanova <juanluiscr27@hotmail.com>
2023-05-22 23:57:13 -0400

    start to Run Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,5 @@ import 'package:flutter/material.dart';
 
 // program entry point
 void main() {
-  // Flutter global function that display a Widget argument to the screen
-  runApp(const MyFlutterApp());
-}
-
-// Stateless Widget is a Flutter class used for UI elements with no dynamic data
-class MyFlutterApp extends StatelessWidget {
-  const MyFlutterApp({super.key});
-
-  // The build method returns a Widget and is called any time Flutter needs to
-  // rebuild the UI
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  runApp(MyApp);
 }
